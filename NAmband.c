2ddd90e5c2b9d04191147b56035cbfe188184b53
Sara Pacheco de Azevedo <thesarapacheco@gmail.com>
2024-10-03 16:33:05 -0300

    Create NAmband.c

---
+++ NAmband.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int N, num, result;
+    
+    while (1) {
+        scanf("%d", &N);
+        if (N == -1) break;
+        result = 0;
+        for (int i = 0; i < N; i++) {
+            scanf("%d", &num);
+            result ^= num;
+        }
+        printf("%d\n", result);
+    }
+    
+    return 0;
+}
